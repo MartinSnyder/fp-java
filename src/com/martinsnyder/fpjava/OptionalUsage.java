3777aca8cc33c4e118027f4091b509cbd9e8148f
Martin Snyder <github@martinsnyder.net>
2019-11-04 21:07:34 -0500

    Expand optional examples

--- src/com/martinsnyder/fpjava/OptionalUsage.java
+++ src/com/martinsnyder/fpjava/OptionalUsage.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 interface OptionalUsage {
     static Optional<Double> divide(double numerator
-                                  , double denominator) {
+                                  ,double denominator) {
         return denominator == 0
                 ? Optional.empty()
                 : Optional.of(numerator / denominator);
@@ -13,5 +13,35 @@ interface OptionalUsage {
     static void main(String[] args) {
         System.out.println("1 / 0 = " + divide(1, 0));
         System.out.println("1 / 3 = " + divide(1, 3));
+    }
+
+    // Use optional.orElse(defaultValue) instead
+    static int safeGet(Optional<Integer> optional, int defaultValue) {
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        else {
+            return defaultValue;
+        }
+    }
+
+    // Use optional.map(Object::toString) instead
+    static Optional<String> safeToString(Optional<Integer> optional) {
+        if (optional.isPresent()) {
+            return Optional.of(optional.get().toString());
+        }
+        else {
+            return Optional.empty();
+        }
+    }
+
+    // Use numerator.flatMap(i -> divide(i, denominator)) instead
+    static Optional<Double> safeDivide(Optional<Integer> numerator, int denominator) {
+        if (numerator.isPresent()) {
+            return divide(numerator.get(), denominator);
+        }
+        else {
+            return Optional.empty();
+        }
     }
 }
